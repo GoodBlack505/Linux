9510015f5edefec055f1882c3d770987f77afe65
GoodBlack5500 <136597064+GoodBlack5500@users.noreply.github.com>
2023-08-07 17:28:45 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,27 @@
+
+sudo apt-get full-upgrade -y
+clear
+sudo apt install git -y
+clear
+sudo apt install snapd -y
+clear
+killall snap-store
+snap refresh
+clear
+sudo apt install remmina -y
+clear
+sudo apt install vlc -y
+clear
+sudo apt install python3-pip -y
+clear
+sudo apt install checkinstall -y
+clear
+sudo apt install gdebi -y
+clear
+sudo snap install code
+clear
+sudo apt-get install vinagre -y
+clear
+reboot
+
+
